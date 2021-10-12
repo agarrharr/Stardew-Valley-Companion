bfef0d75ff605ad73c4b63762637160e81ac0bbd
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2021-10-12 11:44:07 -0600

    Show farm information on screen

--- Stardew Companion/Books.swift
+++ Stardew Companion/Books.swift
@@ -8,6 +8,10 @@
 import Foundation
 import SWXMLHash
 
+func pad2Digit(_ number: Int) -> String {
+    String(format: "%02d", number)
+}
+
 let xmlWithNamespace = readLocalFile(forName: "ExampleSaveFile")
 //let xmlWithNamespace = """
 //<root xmlns:h=\"http://www.w3.org/TR/html4/\"
@@ -34,6 +38,61 @@ let count = xml["root"].all.count
 func getPlayerName() -> String {
     xml["SaveGame"]["player"]["name"].element!.text
 }
+
+func getMoney() -> String {
+    let formatter = NumberFormatter()
+    formatter.usesGroupingSeparator = true
+    formatter.groupingSeparator = ","
+    formatter.groupingSize = 3
+    let value = Int(xml["SaveGame"]["player"]["money"].element!.text)!
+    let number = NSNumber(value: value)
+    let formattedValue = formatter.string(from: number)!
+    
+    return "\(formattedValue) g"
+}
+
+func getFarmName() -> String {
+    "\(xml["SaveGame"]["player"]["farmName"].element!.text) Farm"
+}
+
+func getYear() -> Int {
+    Int(xml["SaveGame"]["year"].element!.text)!
+}
+
+func getDayOfMonth() -> Int {
+    Int(xml["SaveGame"]["dayOfMonth"].element!.text)!
+}
+
+func getCurrentSeason() -> String {
+    xml["SaveGame"]["currentSeason"].element!.text
+}
+
+func getTimeOfYear() -> String {
+    "Day \(getDayOfMonth()) of \(getCurrentSeason().capitalized), Year \(getYear())"
+}
+
+private func getMillisecondsPlayed() -> Int {
+    Int(xml["SaveGame"]["player"]["millisecondsPlayed"].element!.text)!
+}
+
+func getformattedTimePlayed() -> String {
+    let milliseconds = getMillisecondsPlayed()
+    let totalMinutes = milliseconds / 60000
+    let hours = totalMinutes / 60
+    let minutes = totalMinutes % 60
+    return "\(pad2Digit(hours)):\(pad2Digit(minutes))"
+}
+
+func getFarmType() -> String {
+    let farmTypes = ["Standard farm", "River farm", "Forest farm", "Hill-top farm", "Wilderness farm"]
+
+    return farmTypes[Int(xml["SaveGame"]["whichFarm"].element!.text)!]
+}
+
+// ["questLog"]["Quest"]
+// <_currentObjective>Build a Coop.</_currentObjective><_questDescription>Robin, the local carpenter, lives north of town. In exchange for raw materials and money, she'll construct new buildings on your farm. You'll need her to build a coop or barn so that you can raise animals.</_questDescription><_questTitle>Raising Animals</_questTitle><rewardDescription>-1</rewardDescription><completionString>Coop</completionString><accepted>false</accepted><completed>true</completed><dailyQuest>false</dailyQuest><showNew>false</showNew><canBeCancelled>false</canBeCancelled><destroy>false</destroy><id>7</id><moneyReward>100</moneyReward><questType>8</questType><daysLeft>0</daysLeft><nextQuests><int>-1</int></nextQuests><questTitle>Raising Animals</questTitle>
+
+
 
 // enumerate all child elements (procedurally)
 // enumerate(indexer: xml, level: 0)
--- Stardew Companion/ContentView.swift
+++ Stardew Companion/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text(getPlayerName())
-            .padding()
+        Text(getFarmName())
+        Text(getFarmType())
+        Text(getMoney())
+        Text(getTimeOfYear())
+        Text(getformattedTimePlayed())
     }
 }
 
