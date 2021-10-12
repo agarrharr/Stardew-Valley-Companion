5faa7a708ef78ff32be12e8bd4fd676c920d503b
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2021-10-12 09:18:39 -0600

    Initial Commit

---
+++ Stardew Companion/Stardew_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stardew_CompanionApp.swift
+//  Stardew Companion
+//
+//  Created by Adam Garrett-Harris on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Stardew_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
